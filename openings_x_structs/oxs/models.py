809c5085a2849d859859fd199510667126bc4176
JuanMaAl <juanmaal1986@gmail.com>
2024-11-06 04:48:31 +0100

    Models and migrations

--- openings_x_structs/oxs/models.py
+++ openings_x_structs/oxs/models.py
@@ -2,7 +2,31 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	name=models.CharField(max_length=100)
+	name = models.CharField(max_length = 100, unique = True)
+	def __str__(self):
+		return self.name
 
-class Test(models.Model):
-	name=models.CharField(max_length=25)
+class Repertoires(models.Model):
+	repertoire_name = models.CharField(max_length = 30)
+	user = models.ForeignKey(User, on_delete = models.CASCADE, default = 1)
+	def __str__(self):
+		return self.repertoire_name
+
+class RepertoireVariants(models.Model):
+	structure = models.CharField(max_length = 30)
+	moves = models.CharField(max_length = 300)
+	repertoires = models.ManyToManyField(Repertoires)
+	def __str__(self):
+		return self.moves
+
+class Structures(models.Model):
+	structure_name = models.CharField(max_length = 30, unique = True)
+	structure_description = models.CharField(max_length = 300)
+	def __str__(self):
+		return self.structure_name
+	
+class Variants(models.Model):
+	moves = models.CharField(max_length = 300)
+	structures = models.ForeignKey(Structures, on_delete = models.CASCADE, default = 1)
+	def __str__(self):
+		return self.moves
