a714aaee4db8a7e5db4e56d079a41c4d35532c3f
JuanMaAl <juanmaal1986@gmail.com>
2024-12-17 14:32:54 +0100

    simply view and url try with hello juan

---
+++ oxs/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('hello/', views.hello_juan, name='hello_juan'),
+]
--- oxs/views.py
+++ oxs/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Structures
+
 
 # Create your views here.
+
+#def structures(request):
+#	structures = Structures.object.all()
+#	return render(request, 'structures.html', {'structures': structures})
+
+def hello_juan(request):
+	return HttpResponse("Hello, Juan!")
