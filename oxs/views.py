68e05bbc89d06a644d99860084e4df9a1dc93f7e
JuanMaAl <juanmaal1986@gmail.com>
2024-12-28 07:58:24 +0100

    new repertoire form for create an entry in repertoires table, not complete

--- oxs/views.py
+++ oxs/views.py
@@ -11,6 +11,22 @@ def home(request, user_name):
 	return render(request, 'home.html', 
 	{'user': user, 'repertoires': repertoires})
 
+def new_repertoire(request, user_name):
+	user = User.objects.filter(name = user_name)
+	return render(request, 'new_repertoire.html', { 'user' : user})
+
+def process_new_repertoire(request):
+	if request.method =='POST':
+		repertoire_name = request.POST.get('repertoire_name')
+		repertoire_color = request.POST.get('repertoire_color')
+		user = request.POST.get('user')
+		repertoire = Repertoires(repertoire_name=repertoire_name,
+		repertoire_color=repertoire_color, user=user)
+		repertoire.save()
+		return HttpResponse("Nuevo Repertorio Creado")
+	else:
+		return HttpResponse("Error al crear el repertorio")
+
 def structures(request):
 	structures = Structures.objects.all()
 	return render(request, 'structures.html', {'structures': structures})
